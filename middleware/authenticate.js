7447550a83fe7dd2ac4af77b1c7d5fd1163375c5
Dishant1901 <dishantsinghom@gmail.com>
2024-05-11 09:27:45 +0530

    Authentication Middleware added

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,38 +1,58 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/userSchema");
+const User = require("../models/user");
+const { default: axios } = require("axios");
 
 const verifyToken = async (req, res, next) => {
-    let token;
+    let tokenString;
     // console.log(req.headers);
     if (
         req.headers.authorization &&
         req.headers.authorization.startsWith("Bearer")
     ) {
-        token = req.headers.authorization.split(" ")[1];
+        tokenString = req.headers.authorization.split(" ")[1];
     }
 
 
-    // const token =
-    //     req.body.token || req.query.token || req.headers["x-access-token"];
-
-    // console.log(token);
-    if (!token) {
+    if (!tokenString) {
         return res.status(403).send("A token is required for authentication");
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
-        const user = await User.findById(decoded.id);
-
+        const decoded = jwt.verify(tokenString, process.env.ACCESS_SECRET);
+        const user = await User.findById(decoded.trackItUserId);
+        // console.log(decoded)
+        // console.log(user)
         if (!user) {
             return res.status(420).json({ error: "No user found with this id" });
         }
-        if (user.accessToken !== token)
+        if (user.accessToken !== tokenString)
             return res.status(420).json({ error: "Unable to authorize. Please login" });
-        req.user = user;
+        
+        const token = user.accessToken;
+
+        // Making call to monnit service for authentication of token
+        try {
+            // console.log(token)
+            MONNIT_URL = process.env.MONNIT_URL
+
+            const response = await  axios.post(
+                MONNIT_URL+'/api/token-auth',
+                {token},
+
+            )
+
+            // console.log(response.data.user)
+            req.user = response.data.user
+            req.trackItUserId = decoded.trackItUserId
+            req.user.accessToken = user.accessToken
+
+        } catch (error) {
+            return res.status(500).json({message: error.message})
+        }
 
         next();
     } catch (err) {
+        console.log(err)
         return res.status(404).json({ error: "Not authorized " });
     }
 };
