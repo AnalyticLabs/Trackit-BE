a26e2540aa5af76ddcc1f3c4d267e120beed2b63
Dishant1901 <dishantsinghom@gmail.com>
2024-06-04 22:34:38 +0530

    Backlog search API

--- controllers/backlog.js
+++ controllers/backlog.js
@@ -39,3 +39,40 @@ exports.getBacklog = async(req,res) =>{
     }
 }
 
+exports.searchBacklog = async(req,res) =>{
+    try {
+        const {search, projectId} = req.query
+        if(!search || !projectId) {
+            return res.status(400).json({success:false,message:"Please Provide Both Query Parameter"})
+        }
+
+        const tasks = await Task.find({title:{$regex:search,$options:'i'},projectId})
+        .select('title _id ')
+
+        const condition = tasks.map(task => task.id)
+
+        const backlogs = await Backlog.find({projectId, task: { $in:condition }})
+        .populate({
+            path:"task",select:" status title description type assignee storyId linkedTask sprintId tags logTime priority expectedTime ",
+            populate:[
+                {path:"linkedTask", select:"title status tags"},
+                {path:"sprintId", select:"name"},
+                {
+                    path:"storyId", select:"title description epicId assignee",
+                    populate:[
+                        {
+                            path:"epicId", select:"title description assignee"
+                        }
+                    ]
+                }
+            ]
+        })
+        
+
+        return res.status(200).json({success:true,backlogs})
+    } catch (error) {
+        // console.log(error)
+        return res.status(500).json({success:false,message:"Internal Server Error"})
+    }
+}
+
