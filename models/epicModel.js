4fec86962089fde11d119eeed0cdcc1a569e842c
Dishant1901 <dishantsinghom@gmail.com>
2024-06-05 17:21:41 +0530

    Removed unique true field

--- models/epicModel.js
+++ models/epicModel.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const epicSchema = new mongoose.Schema({
     title:{
         type: String,
-        unique: true,
         required: [true, "Please Provide epic title"]
     },
 
